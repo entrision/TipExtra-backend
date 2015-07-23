fe6456f935daec269d5703e2074e9b6438fe1784
James T. Perreault <james@perreau.lt>
2015-07-23 15:31:09 -0400

    remove braintree card handling methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,30 +8,7 @@ class User < ActiveRecord::Base
 
   after_create :braintree_create_customer
 
-  #has_many :drinks, through: :orders
   has_many :orders, dependent: :destroy
-
-  def braintree_update_card(card_stuff)
-  end
-
-  def braintree_add_card(credit_card)
-    result = Braintree::Customer.update(
-      self.braintree_customer_id,
-      credit_card: {
-        number: credit_card[:number],
-        cvv:    credit_card[:cvv],
-        expiration_month: credit_card[:expiration_month],
-        expiration_year: credit_card[:expiration_year],
-        cardholder_name: credit_card[:cardholder_name]
-      }
-    )
-    unless result.success?
-      p result.errors
-    end
-  end
-
-  def braintree_remove_card(card_stuff)
-  end
 
   private
 
