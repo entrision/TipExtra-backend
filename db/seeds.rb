5ba70dde085a14c5490cc68cb10b54d73156707c
James T. Perreault <james@perreau.lt>
2015-07-27 11:37:04 -0400

    seed menu, drinks, images, user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+ipad_user = User.create!(email: 'admin@tipextra.com', password: 't1p3xtr4!', first_name: 'Tip', last_name: 'Extra')
+menu = Menu.create!(name: 'The Menu', user: ipad_user)
+
+rc = Drink.create!(name: 'Rum & Coke', price: 600, menu: menu)
+mr = Drink.create!(name: 'Gin Martini', price: 900, menu: menu)
+bl = Drink.create!(name: 'Bud Light', price: 400, menu: menu)
+
+i1 = Image.create!(image: File.open(Rails.root + 'spec/support/drink_images/rum_coke.jpg'))
+i1.drink = rc
+i1.save
+
+i2 = Image.create!(image: File.open(Rails.root + 'spec/support/drink_images/martini.jpg'))
+i2.drink = mr
+i2.save
+
+i3 = Image.create!(image: File.open(Rails.root + 'spec/support/drink_images/bud_light_bottle.jpg'))
+i3.drink = bl
+i3.save
